05bc58c8468008f1e97b721e19478193186945dc
codeblood10 <ankitshr8@gmail.com>
2017-07-14 03:23:40 +0530

    intital commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const hbs = require("hbs");
+var app = express();
+
+var fs = require('fs');
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set("view engine" ,"hbs");
+
+app.use((req,res,next)=>
+{ var now = new Date().toString();
+  var log = `${now}:${req.method},${req.url}`;
+  console.log(log);
+   fs.appendFile('server.log',log +"\n",(err) =>{
+     if(err)
+  console.log("unable to perform request");
+    });
+  next();
+});
+//app.use((req,res,next)=>
+//{
+  //res.render("maintenance.hbs");
+
+//}); this is use to say that site is at maintenance
+app.use(express.static(__dirname+'/public')); // maintenance was override du to this
+hbs.registerHelper("x",()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamit",(text) =>{
+   return text.toUpperCase();
+});
+
+app.get("/",(req,res) => {
+    // res.send("</h1> world</h1>");
+    res.render('home.hbs',{
+      title:"home page" ,
+      msg :"hey bud u wanna get some ",
+
+    });
+ });
+ app.get("/about",(req,res)=>
+ {
+  res.render('about.hbs',{
+  pagetitle:"aboutpage",
+
+  });
+ });
+ app.get("/bad" ,(req,res)=>
+{
+
+  res.send("sorry buddy  cant send it");
+});
+ app.listen(3000 ,()=>{
+ console.log("server is up on port  3000");
+  });
